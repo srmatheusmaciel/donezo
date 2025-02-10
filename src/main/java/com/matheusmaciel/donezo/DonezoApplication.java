c4874837e546504962126a74ac6454639a7f0570
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-10 09:39:42 -0300

    Criação do projeto e definição das primeiras dependencias
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

---
+++ src/main/java/com/matheusmaciel/donezo/DonezoApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmaciel.donezo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonezoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonezoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/matheusmaciel/donezo/user/UserModel.java
@@ -0,0 +1,14 @@
+package com.matheusmaciel.donezo.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+  
+  private String username;
+  private String password;
+  private String name;
+
+
+
+}
