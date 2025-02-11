0b6acc7cc70bca1fb565c68c9c56908e9f581d83
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-11 08:53:53 -0300

    validando username
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/donezo/user/UserController.java
+++ src/main/java/com/matheusmaciel/donezo/user/UserController.java
@@ -15,9 +15,14 @@ public class UserController {
 
   @PostMapping("/")
   public UserModel create(@RequestBody UserModel userModel) {
+      var user = this.userRepository.findByUsername(userModel.getUsername());
+      if (user != null) {
+       System.out.println("Usuário Já Existe");
+       return null;
+      }
+
       var userCreated = this.userRepository.save(userModel);
       return userCreated;
+  
     }
-  
-  
 }
--- src/main/java/com/matheusmaciel/donezo/user/UserRepository.java
+++ src/main/java/com/matheusmaciel/donezo/user/UserRepository.java
@@ -5,5 +5,5 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
-
+      UserModel findByUsername(String username);
 }
