ad20aa341107f2451eebb96db7e1523c22e05a48
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-12 13:51:44 -0300

    Realizando Filtro
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/donezo/filter/FilterTaskAuth.java
+++ src/main/java/com/matheusmaciel/donezo/filter/FilterTaskAuth.java
@@ -1,25 +1,49 @@
 package com.matheusmaciel.donezo.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    
-        System.out.println("FilterTaskAuth");
-    
-        chain.doFilter(request, response);
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+       
+        // Pegar a autenticação (usuario e senha)
+        var authorization =  request.getHeader("Authorization");
+
+        //remover a palavra Basic que fica escrito no authorization
+        var authEncoded  = authorization.substring("Basic".length()).trim();
+       
+        // fazer um decode dos caracteres
+        byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+        
+        // converter para string
+        var authString  = new String(authDecoded);
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+        
+        // Validar usuario
+
+        //Validar a senha
+
+        //prosseguir 
+        
+        filterChain.doFilter(request, response);
   }
+
+ 
 
 }
