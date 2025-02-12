b4ca5143a6fc374cd59d97a7d658fcd96b155c1e
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-12 11:10:43 -0300

    Criando tabela task
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

---
+++ src/main/java/com/matheusmaciel/donezo/user/Task/TaskModel.java
@@ -0,0 +1,38 @@
+package com.matheusmaciel.donezo.user.Task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "tb_tasks")
+@Data
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+
+  @Column(length = 50, nullable = false)
+  private String title;
+  @Column(length = 240, nullable = false)
+  private String description;
+
+  
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+
+  private String priority;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  private UUID userId;
+
+}
