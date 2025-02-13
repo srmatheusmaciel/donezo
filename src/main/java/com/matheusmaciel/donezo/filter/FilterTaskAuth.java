9f3ffc07f6ccae7b10c7d14171d150d64aea76f2
Matheus Maciel <srmatheusmaciel@hotmail.com>
2025-02-13 17:41:18 -0300

    decode de senha
    Signed-off-by: Matheus Maciel <srmatheusmaciel@hotmail.com>

--- src/main/java/com/matheusmaciel/donezo/filter/FilterTaskAuth.java
+++ src/main/java/com/matheusmaciel/donezo/filter/FilterTaskAuth.java
@@ -3,9 +3,13 @@ package com.matheusmaciel.donezo.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.matheusmaciel.donezo.user.UserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +19,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private UserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -36,12 +43,24 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         String password = credentials[1];
         
         // Validar usuario
-
-        //Validar a senha
-
-        //prosseguir 
+          var user = this.userRepository.findByUsername(username);
+          if(user == null) {
+            response.sendError(401);
+          } else{
+            
+            //Validar a senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if(passwordVerify.verified){
+              filterChain.doFilter(request, response);
+            } else {
+              response.sendError(401);
+            }
+          }
         
-        filterChain.doFilter(request, response);
+
+          //prosseguir 
+        
+        
   }
 
  
