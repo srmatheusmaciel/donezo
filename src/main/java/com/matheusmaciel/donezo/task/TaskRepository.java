4dac3d4c28702d052fec16efa0a4c39f116f90d6
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-02-15 11:55:45 -0300

    Listando tarefas
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

--- src/main/java/com/matheusmaciel/donezo/task/TaskRepository.java
+++ src/main/java/com/matheusmaciel/donezo/task/TaskRepository.java
@@ -1,9 +1,10 @@
 package com.matheusmaciel.donezo.task;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByUserId(UUID userId);
 }
