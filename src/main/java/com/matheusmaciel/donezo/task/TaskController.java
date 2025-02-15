a2d076e25010b16b286c694a7852ba75cb279628
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-02-15 11:43:42 -0300

    Validação das horas
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

--- src/main/java/com/matheusmaciel/donezo/task/TaskController.java
+++ src/main/java/com/matheusmaciel/donezo/task/TaskController.java
@@ -1,8 +1,11 @@
 package com.matheusmaciel.donezo.task;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +20,19 @@ public class TaskController {
   private TaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+  public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
     var userId = request.getAttribute("userId");
     taskModel.setUserId((UUID) userId);
+   
+    // 15/02/2025 - Current date
+    // 15/02/2025 - Start date
+
+    var currentDate = LocalDateTime.now();
+    if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Start date / end date must be in the future");
+    }
+  
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.OK).body(task);
   } 
 }
